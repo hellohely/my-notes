248ecce424828f53b0f63592cc003f20241ad059
Henrietta Lybeck <52459904+hellohely@users.noreply.github.com>
2022-06-11 13:31:57 +0200

    Editor only shows if you are logged in

--- frontend/src/components/documenteditor.js
+++ frontend/src/components/documenteditor.js
@@ -26,7 +26,26 @@ export default function DocumentEditor() {
       {loggedIn && (
         <>
           {" "}
-          <h1>Inloggad tjoho</h1>
+          <Editor
+        apiKey='9kynm4z26f02t3tjxi2j4u2bmc5aphhddldyg9qp8t2krq6t'
+        onInit={(evt, editor) => editorRef.current = editor}
+        initialValue="<p>This is the initial content of the editor.</p>"
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+            'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+          ],
+          toolbar: 'undo redo | blocks | ' +
+            'bold italic forecolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+        }}
+      />
+      <button onClick={log}>Log editor content</button>
         </>
       )}
       {!loggedIn && (
